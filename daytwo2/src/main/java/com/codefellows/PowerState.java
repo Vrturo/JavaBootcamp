651bafc36be1fb3b844c2c6fc9517164e0f7e59d
Arturo Perez <VrturoPerez@gmail.com>
2017-10-16 21:05:27 -0700

    complex enums

--- daytwo2/src/main/java/com/codefellows/PowerState.java
+++ daytwo2/src/main/java/com/codefellows/PowerState.java
@@ -1,9 +1,23 @@
 package com.codefellows;
 
 public enum PowerState {
-    ON,
-    OFF,
-    SUSPENDED,
-    HIBERNATED,
-    UNKNOWN
+    ON("power is on"),
+    OFF("power is off"),
+    SUSPENDED("suspended"),
+    HIBERNATED("hibernated"),
+    UNKNOWN("unknown");
+
+    private String description;
+
+    private PowerState() {
+        description = null;
+    }
+
+    private PowerState(String d) {
+        description = d;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
