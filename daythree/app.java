832c19a3fdf21f3ad64fad1feed7799c4bdb03d9
Perez <perezart@amazon.com>
2017-10-18 19:36:15 -0700

    create lambda expression

--- daythree/app.java
+++ daythree/app.java
@@ -7,6 +7,29 @@ public class App
     {
         List<String> myStrings = Arrays.asList(new String[] {"hello", "goodbye", "Brad", "Farr"})
 
-        
+        private static void doForEachString(List<String> strings, String analyzer,
+            String subString) {
+            for (String s : strings) {
+                if (analyzer.analyze(s, "b"))
+            }
+        }
+
+        doForEachString(myStrings, (x, y) -> {
+            if (x.contains(y)) {
+                return true;
+            }
+            return false;
+        }, "b");
+
+        @FunctionalInterface
+        private interface IStringAnalyzer {
+            boolean analyze(String mystring, String subString);
+        }
+
+        private static class stringTraverser {
+            public boolean analyze(String myString, String substring) {
+                return myString.contains(substring);
+            }
+        }
     }
 }
