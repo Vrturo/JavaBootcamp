1e36e0b1b13667298ec577fe06fa846bcb3b8ffb
Perez <perezart@amazon.com>
2017-10-18 19:10:36 -0700

    create day three module

---
+++ daythree/app.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App
+{
+    public static void main( String[] args)
+    {
+        List<String> myStrings = Arrays.asList(new String[] {"hello", "goodbye", "Brad", "Farr"})
+
+        
+    }
+}
